4e3ddb4994e36ded4781307fcc2809dcf614ba4f
mvishnuprasad <vpm735@gmail.com>
2024-07-08 10:25:46 +0530

    customise calendar

--- lib/components/calendar/calendar_view.dart
+++ lib/components/calendar/calendar_view.dart
@@ -1,9 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_week_view/flutter_week_view.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:taskit/components/cards/task_card.dart';
+import 'package:taskit/components/cards/text_view.dart';
 import 'package:taskit/constants/appconstants.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -36,69 +37,123 @@ class CalendarView extends ConsumerWidget {
                   shouldFillViewport: true,
                   firstDay: DateTime.utc(2010, 10, 16),
                   lastDay: DateTime.utc(2030, 3, 14),
-                  focusedDay: DateTime.now(),
+                  focusedDay: selectedDate.state,
                   currentDay: selectedDate.state,
                   rowHeight: 52,
                   headerVisible: true,
-                  daysOfWeekStyle: const DaysOfWeekStyle(),
                   headerStyle: HeaderStyle(
                       decoration: BoxDecoration(
                           shape: BoxShape.rectangle,
                           color: AppThemeColors.highLight,
                           borderRadius: const BorderRadius.only(
                               topLeft: Radius.circular(20),
-                              topRight: Radius.circular(20)))),
+                              topRight: Radius.circular(20))),
+                      titleTextFormatter: (time, value) {
+                        return DateFormat('MMMM yyyy').format(time);
+                      },
+                      leftChevronIcon: Icon(
+                        Icons.chevron_left,
+                        color: AppThemeColors.background,
+                      ),
+                      rightChevronIcon: Icon(
+                        Icons.chevron_right,
+                        color: AppThemeColors.background,
+                      ),
+                      titleTextStyle: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              color: AppThemeColors.background,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold)),
+                      formatButtonShowsNext: false,
+                      formatButtonTextStyle: GoogleFonts.poppins(
+                          textStyle: TextStyle(
+                              color: AppThemeColors.background,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500)),
+                      formatButtonDecoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(10)),
+                          border: Border.all(
+                            color: AppThemeColors.background,
+                          ))),
                   onDaySelected: (selectedDay, focusedDay) {
-                    ref.read(dateProvider.notifier).state = selectedDay;
-                    selectedDay = focusedDay;
+                    selectedDate.state = selectedDay;
+                    // selectedDay = focusedDay;
                     myBool.value = !myBool.value;
                   },
                   calendarStyle: CalendarStyle(
-                      selectedTextStyle: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                              color: AppThemeColors.highLight,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold)),
-                      defaultTextStyle: GoogleFonts.poppins(
-                          textStyle: TextStyle(
-                              color: AppThemeColors.primaryColor,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold)),
-                      selectedDecoration: BoxDecoration(
-                          //shape: BoxShape.r,
-                          color: AppThemeColors.highLight,
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(20))),
-                      todayDecoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: AppThemeColors.highLight,
-                        //borderRadius: const BorderRadius.all(Radius.circular(20))
-                      )),
+                    selectedTextStyle: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                            color: AppThemeColors.highLight,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold)),
+                    defaultTextStyle: GoogleFonts.poppins(
+                        textStyle: TextStyle(
+                            color: AppThemeColors.primaryColor,
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold)),
+                  ),
                   calendarBuilders: CalendarBuilders(
-                    selectedBuilder: (context, selectedDay, focusedDay) {
-                      if (selectedDay ==
-                          ref.read(dateProvider.notifier).state) {
-                        final text = ref.read(dateProvider.notifier).state;
-                        return Container(
-                          decoration: BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              color: AppThemeColors.highLight,
-                              borderRadius: const BorderRadius.only(
-                                  topLeft: Radius.circular(20),
-                                  topRight: Radius.circular(20))),
-                          child: Center(
-                            child: Text("$text"),
+                    defaultBuilder: (context, day, date) {
+                      if (day.weekday == DateTime.sunday) {
+                        return Center(
+                          child: TextView(
+                            title: DateFormat('dd').format(day),
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red,
+                          ),
+                        );
+                      } else {
+                        return Center(
+                          child: TextView(
+                            title: DateFormat('dd').format(day),
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: AppThemeColors.primaryColor,
                           ),
                         );
                       }
                     },
+                    todayBuilder: (context, day, date) {
+                      return Center(
+                        child: Container(
+                          padding: const EdgeInsets.all(13),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppThemeColors.primaryColor,
+                          ),
+                          child: TextView(
+                            title: DateFormat('dd').format(day),
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: AppThemeColors.background,
+                          ),
+                        ),
+                      );
+                    },
                     dowBuilder: (context, day) {
-                      if (day.weekday == DateTime.sunday) {
-                        final text = DateFormat.E().format(day);
+                      final weekday = DateFormat.E().format(day);
+                      if (weekday == "Sun") {
                         return Center(
                           child: Text(
-                            text,
-                            style: const TextStyle(color: Colors.red),
+                            weekday,
+                            style: GoogleFonts.poppins(
+                                textStyle: const TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600)),
+                          ),
+                        );
+                      } else {
+                        return Center(
+                          child: Text(
+                            weekday,
+                            style: GoogleFonts.poppins(
+                                textStyle: TextStyle(
+                                    color: AppThemeColors.primaryColor,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w600)),
                           ),
                         );
                       }
@@ -112,8 +167,18 @@ class CalendarView extends ConsumerWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    TaskCard(width: width, task: 'Tasks', height: height, number: 08,),
-                    TaskCard(width: width, task: 'Events', height: height, number: 02,),
+                    TaskCard(
+                      width: width,
+                      task: 'Tasks',
+                      height: height,
+                      number: 08,
+                    ),
+                    TaskCard(
+                      width: width,
+                      task: 'Events',
+                      height: height,
+                      number: 02,
+                    ),
                     //TodayDate()
                   ],
                 ));
